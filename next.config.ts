0bfd5d0733658637d08049ab9131bd8400046fed
tannerlee119 <tanner.lee2022@gmail.com>
2025-07-21 10:39:38 -0500

    Add support for external images from social media platforms in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,36 @@ const nextConfig: NextConfig = {
     return config;
   },
   
+  // Allow external images from social media platforms
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.fbcdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.cdninstagram.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.instagram.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.tiktokcdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.ytimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+      },
+    ],
+  },
+  
   // Improve development experience
   experimental: {
     // Reduce memory usage in development
